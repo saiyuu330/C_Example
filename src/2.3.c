9825ebfecbae8608c2f954b5034d224fe5102ef7
윤재웅 <jwyun0330@naver.com>
2020-08-10 11:04:55 +0000

    2.3.c

---
+++ src/2.3.c
@@ -0,0 +1,9 @@
+// Fig. 2.3: fig02_03.c
+// Printing on one line with two printf statements.
+#include <stdio.h>
+
+// function main begins program execution
+int main(void){
+	printf("Welcom ");
+	printf("to C!\n");
+} // end function main
