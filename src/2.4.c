43a9ff476c4515ea16f24e8a342325dcbb18b3d4
윤재웅 <jwyun0330@naver.com>
2020-08-11 11:01:47 +0000

    08.11 업데이트

--- src/2.4.c
+++
@@ -1,8 +0,0 @@
-// Fig. 2.4: fig02_04.c
-// Printing multiple line with a single printf.
-#include <stdio.h>
-
-//function main begins program execution
-int main(void){
-	printf("Welcome\nto\nC!\n");
-} // end function main
