a0d93e49118512d4182c156642b788957af7f8e3
윤재웅 <jwyun0330@naver.com>
2020-08-11 10:58:51 +0000

    파일 이름 변경

---
+++ src/2.04.c
@@ -0,0 +1,8 @@
+// Fig. 2.4: fig02_04.c
+// Printing multiple line with a single printf.
+#include <stdio.h>
+
+//function main begins program execution
+int main(void){
+	printf("Welcome\nto\nC!\n");
+} // end function main
